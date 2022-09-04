f507a3bab4e1d2a8bd6aedbd0de03c5abc7ebe43
Michael <mescamilla1114@gmail.com>
2022-09-04 12:47:44 -0500

    commit tree 50f603773ec4f8485921b8d6e0ee63d885ad3742

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const surprise = ["Guadalupe", "Ollie", "Aki"];
+const newAr = [];
+
+function writeCards (surprise, newAr){
+
+    for(let i = 0;i <surprise.length;i++){
+        newAr[i]=`Thank you, ${surprise[i]}, for the wonderful surprise gift!`;
+        newAr.length++;
+    }
+    return(newAr);
+
+}
+
+function countDown(){
+    let cDown;
+while (cDown>0) {
+  console.log(cDown++);
+}
+}
